07cd28fd790d47b2f1ca98091d17ea661da49494
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-22 12:27:01 +0300

    Add feature supporting HTTPS
    - Handle dealing with websites use HTTPS

--- modules/URL.py
+++ modules/URL.py
@@ -1,16 +1,26 @@
 import socket
+import ssl
 
 
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split('://', 1)
-        assert self.scheme == 'http'
+        assert self.scheme in ['http', 'https']
 
         if "/" not in url:
             url = url + "/"
 
         self.host, url = url.split('/', 1)
         self.path = "/" + url
+
+        if self.scheme == 'http':
+            self.port = 80
+        elif self.scheme == 'https':
+            self.port = 443
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
 
     def request(self):
         s = socket.socket(
@@ -19,7 +29,11 @@ class URL:
             proto=socket.IPPROTO_TCP,
         )
 
-        s.connect((self.host, 80))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        s.connect((self.host, self.port))
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
         request += "\r\n"
