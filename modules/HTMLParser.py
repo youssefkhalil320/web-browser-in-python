c8d6ff6719aff2f4d50aef4b53ebc75a81d09289
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-29 22:12:56 +0300

    Add HTMLParser

---
+++ modules/HTMLParser.py
@@ -0,0 +1,23 @@
+class HTMLParser:
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+
+    def parse(self):
+        text = ""
+        in_tag = False
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text:
+                    self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+        return self.finish()
