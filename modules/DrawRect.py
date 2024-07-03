a451c4f04e396f5c00c322e4adf42b5a591f356e
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-07-03 21:59:30 +0300

    Add DrawRect module

---
+++ modules/DrawRect.py
@@ -0,0 +1,22 @@
+from .wbetools import js_hide
+
+
+class DrawRect:
+    def __init__(self, x1, y1, x2, y2, color):
+        self.top = y1
+        self.left = x1
+        self.bottom = y2
+        self.right = x2
+        self.color = color
+
+    def execute(self, scroll, canvas):
+        canvas.create_rectangle(
+            self.left, self.top - scroll,
+            self.right, self.bottom - scroll,
+            width=0,
+            fill=self.color)
+
+    @js_hide
+    def __repr__(self):
+        return "DrawRect(top={} left={} bottom={} right={} color={})".format(
+            self.top, self.left, self.bottom, self.right, self.color)
